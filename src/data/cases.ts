396c0dba2ea38afb9486c86e4102118782f83203
Takeshi Takatsudo <takazudo@gmail.com>
2025-09-11 02:31:18 +0900

    typeA/typeB

--- src/data/cases.ts
+++ src/data/cases.ts
@@ -1,8 +1,23 @@
 import type { Cases } from '../types';
 
 export const cases: Cases = {
-  'zudo-block-40': {
-    name: 'zudo-block-40',
+  'zudo-block-40-type-a': {
+    name: 'zudo-block-40 Aタイプ',
+    hp: 40,
+    material: 'acrylic',
+    panels: [
+      { id: 'side1', name: 'サイド1' },
+      { id: 'side2', name: 'サイド2' },
+      { id: 'front1', name: 'フロント1' },
+      { id: 'front2', name: 'フロント2' },
+      { id: 'bottom1', name: 'ボトム1' },
+      { id: 'bottom2', name: 'ボトム2' },
+      { id: 'back1', name: 'バック1' },
+      { id: 'back2', name: 'バック2' },
+    ],
+  },
+  'zudo-block-40-type-b': {
+    name: 'zudo-block-40 Bタイプ',
     hp: 40,
     material: 'acrylic',
     panels: [
