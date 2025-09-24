e52612fb1c1a40ea1031601dc746de261777359b
Takeshi Takatsudo <takazudo@gmail.com>
2025-09-25 05:21:50 +0900

    wip

--- components/price-table.tsx
+++ components/price-table.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const priceData = [
-  { model: 'zudo-block-40-ACR', lite: '7,980', nuts: '9,980', dual: '12,080', metal: '15,480' },
   { model: 'zudo-block-40-3DP', lite: '4,980', nuts: '6,980', dual: '9,080', metal: '12,480' },
-  { model: 'zudo-block-40x2-ACR', lite: '18,060', nuts: '24,060', dual: '27,360', metal: '37,560' },
+  { model: 'zudo-block-40-ACR', lite: '7,980', nuts: '9,980', dual: '12,080', metal: '15,480' },
   { model: 'zudo-block-40x2-3DP', lite: '15,060', nuts: '21,060', dual: '24,360', metal: '34,560' },
-  { model: 'zudo-block-60-ACR', lite: '8,980', nuts: '11,980', dual: '14,080', metal: '17,480' },
+  { model: 'zudo-block-40x2-ACR', lite: '18,060', nuts: '24,060', dual: '27,360', metal: '37,560' },
   { model: 'zudo-block-60-3DP', lite: '6,980', nuts: '9,980', dual: '12,080', metal: '15,480' },
-  { model: 'zudo-block-60x2-ACR', lite: '19,760', nuts: '28,760', dual: '34,060', metal: '44,260' },
+  { model: 'zudo-block-60-ACR', lite: '8,980', nuts: '11,980', dual: '14,080', metal: '17,480' },
   { model: 'zudo-block-60x2-3DP', lite: '17,760', nuts: '26,760', dual: '32,060', metal: '42,260' },
+  { model: 'zudo-block-60x2-ACR', lite: '19,760', nuts: '28,760', dual: '34,060', metal: '44,260' },
   { model: '10BOX-3DP', lite: '19,680', nuts: '25,680', dual: '28,880', metal: '35,680' },
 ];
 
@@ -20,8 +20,10 @@ export function PriceTable() {
           <tr>
             <th
               rowSpan={2}
-              className="bg-[#a8c8e1] text-black text-left py-2 px-4 border border-gray-400"
-            ></th>
+              className="bg-[#a8c8e1] text-black py-2 px-4 border border-gray-400 text-center"
+            >
+              ケースのモデル
+            </th>
             <th
               colSpan={4}
               className="bg-[#a8c8e1] text-black text-center py-2 px-4 border border-gray-400"
