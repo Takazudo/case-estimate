d94b74c7059879bcd2e304a5889726878fd51eae
Takeshi Takatsudo <takazudo@gmail.com>
2025-10-04 21:59:28 +0900

    dialog + panels gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -5,6 +5,7 @@ import GalleryThumbnailGrid from '@/components/gallery-thumbnail-grid';
 import GalleryDialog from '@/components/gallery-dialog';
 import { useSearchParams } from 'next/navigation';
 import { H1 } from '@/components/article/h1';
+import { P } from '@/components/article/p';
 
 export default function GalleryPage() {
   const searchParams = useSearchParams();
@@ -15,6 +16,13 @@ export default function GalleryPage() {
       <div className="min-h-screen bg-black">
         <div className="py-8">
           <H1>ギャラリー / Gallery</H1>
+          <div className="text-center -mt-vgap-md">
+            <P>
+              ケース関連の写真です。色々ありすぎて分からんという場合、
+              <br />
+              これどれですかなどとURLと共にお気軽にお問い合わせください。
+            </P>
+          </div>
           <GalleryThumbnailGrid items={galleryData} />
         </div>
       </div>
